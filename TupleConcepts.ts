47d04c4c30405571a643468d87400eddfa73833c
Varunraj P V <pv.varunraj.pv@gmail.com>
2026-03-14 22:32:51 +0530

    tuplechanges

--- TupleConcepts.ts
+++ TupleConcepts.ts
@@ -2,4 +2,9 @@
 //tuple vs Array
 //Array: dynamic : runtime more value can be added
 
-let person:[string,number]=["Naveen",23];
+let person:[string,number][]=[["Naveen",23],["Viana",2],["test",23]];
+console.log(person[0]);
+
+let Customer:[string,number]=["Naveen",23];
+Customer.push("addtest",43);
+console.log(Customer);
